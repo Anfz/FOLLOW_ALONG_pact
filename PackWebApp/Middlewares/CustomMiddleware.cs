3cc228a43b364e948946b9827a37a398a2cb25d9
Chris Beaver <chrisbvr@gmail.com>
2017-09-14 20:27:50 +1000

    Initial Custom Middleware

---
+++ PackWebApp/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace PackWebApp.Middlewares
+{
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            _next = next; 
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Debug.WriteLine($" --> Request asked for {httpContext.Request.Path}");
+
+            await _next.Invoke(httpContext);
+        }
+    }
+}
--- PackWebApp/Startup.cs
+++ PackWebApp/Startup.cs
@@ -15,6 +15,7 @@ namespace PackWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,10 @@ namespace PackWebApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc(); 
         }
     }
 }
