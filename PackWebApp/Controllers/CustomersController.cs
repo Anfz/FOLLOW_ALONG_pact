8fa47a15a18a7fc7d6def746e2e435b1247f1ccf
Chris Beaver <chrisbvr@gmail.com>
2017-09-15 20:27:13 +1000

    Simple HttpGet for Customers Controller

---
+++ PackWebApp/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PackWebApp.Dtos;
+using PackWebApp.Entities;
+using PackWebApp.Repositories;
+
+namespace PackWebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/customers")]
+    public class CustomersController : Controller
+    {
+        private ICustomerRepository _customerRepository;
+
+        public CustomersController(ICustomerRepository customersRepository)
+        {
+            _customerRepository = customersRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var allCustomersDto = from customer in _customerRepository.GetAll().ToList()
+                               select Mapper.Map<CustomerDto>(customer);
+            return Ok(allCustomersDto);
+        }
+
+        
+    }
+}
